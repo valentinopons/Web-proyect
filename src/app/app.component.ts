1304ec88d95ab178d1e144e3674f9ec1827807e0
Valentino <valentinopons@gmail.com>
2025-01-08 20:53:06 -0300

    added login frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'log-in', component: LoginComponent}
+];
+export const routing = RouterModule.forRoot(routes);
