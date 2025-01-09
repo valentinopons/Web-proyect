0fbb104e4ebe667bbb78c4ff817620f1a7de08ce
Valentino <valentinopons@gmail.com>
2025-01-09 00:37:35 -0300

    add log in service

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import {FormBuilder,ReactiveFormsModule, Validators} from '@angular/forms';
 import {  } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/auth/login.service';
+import { LoginRequest } from '../../services/auth/login.request';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +13,21 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  private formBuilder = inject(FormBuilder);
-  private router      = inject(Router)
+  private formBuilder  = inject(FormBuilder);
+  private router       = inject(Router);
+  private loginService = inject(LoginService);
   loginForm = this.formBuilder.group({
   email: ['example@gmail.com', [Validators.email, Validators.required]],
   password: ['',[Validators.required]],});
 
   loginValidation(){
     if(this.loginForm.valid){
-      console.log("logged succefully");
       //this.router.navigateByUrl()
+      this.loginService.login(this.loginForm.value as LoginRequest);
       this.loginForm.reset();
     }else{
       alert("Error, invalid credentials")
+      this.loginForm.markAllAsTouched();
     }
   }
 
